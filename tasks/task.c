e32165c15099cf48012e66e729e7cfa83656d04b
feibi-allen <146375798+feibi-allen@users.noreply.github.com>
2024-02-04 17:27:19 +0000

    verification working for letters and negatives

--- tasks/task.c
+++ tasks/task.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 
-int main() {
-    int num1, num2;
+int main(int argc, char *argv[]) {
+    char num1 = atoi(argv[1]), num2 = atoi(argv[2]);
 
     // Prompt the user for input
-    printf("Enter the first number: ");
-    scanf("%d", &num1);
+    //printf("Enter the first number: ");
+    //scanf("%d", &num1);
 
-    printf("Enter the second number: ");
-    scanf("%d", &num2);
+    //printf("Enter the second number: ");
+    //scanf("%d", &num2);
+
+    // Verify Input
+    if (argc != 3 || (num1==0 && !isdigit(*argv[1])) || (num2 == 0 && !isdigit(*argv[2])))
+        {
+            printf("Usage: %s <int1> <int2>\n", argv[0]);
+            return 0;
+        }
+    
 
     // Calculate and print the sum
     int sum = num1 + num2;
